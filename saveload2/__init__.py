41d5af43d7123a4276eeaebf3fdeecae29684fe4
Yu <lkmcfj@outlook.com>
2020-07-06 20:15:00 +0800

    add 2 after saveload

--- saveload2/__init__.py
+++ saveload2/__init__.py
@@ -3,7 +3,7 @@ from . import main
 dependencies = ['mcBasicLib']
 
 def load(log, core):
-    root_dir = os.path.join(core.root_dir, 'saveload')
+    root_dir = os.path.join(core.root_dir, 'saveload2')
     config_filename = os.path.join(root_dir, 'config.json')
     info_filename = os.path.join(root_dir, 'info.json')
     main.SaveLoad(log, core, config_filename, info_filename)
